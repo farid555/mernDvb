8f7decaf821dcab1b4b3d1a19fb5b8be6edc4426
farid555 <mohammad.uzzman2@centria.fi>
2021-09-13 00:55:30 +0300

    login route form validation

--- server/router/auth.js
+++ server/router/auth.js
@@ -56,4 +56,25 @@ router.post('/register', async (req, res) => {
 });
 
 
+//login route
+
+router.post('/signin', async (req, res) => {
+
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ error: "please filled the data..." })
+        }
+        const userLogin = await User.findOne({ email: email });
+        console.log(userLogin);
+        res.json({ message: "user Signin Successfully " })
+
+    } catch (err) {
+        console.log(err);
+
+
+    }
+
+})
+
 module.exports = router;
